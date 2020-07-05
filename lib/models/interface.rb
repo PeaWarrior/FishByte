42730b58ebdaa9d026244c6430db71bcfb45916f
chrisvsanchez <chrisvsanchez@gmail.com>
2020-07-04 22:27:19 -0400

    working on create event method in Interface.rb still in progress

--- lib/models/interface.rb
+++ lib/models/interface.rb
@@ -62,7 +62,7 @@ class Interface
     # --------------------
 
     def event_formatted(event)
-        {name: "\n    #{event.name}".colorize(:blue) + " at " + "#{event.location.name}\n" + "    #{event.date.strftime("%B %d, %Y\n    %A %I:%M %p")}", value: event.id}
+        {name: "\n    #{event.name}".colorize(:blue) + " at " + "#{event.location.name}\n" + "    #{event.date.strftime("%B %d, %Y\n      %A %I:%M %p")}", value: event.id}
     end
     
     def my_events
@@ -132,4 +132,25 @@ class Interface
         puts "Sign up(s) successful! Can't wait to see you there!"
     end
 
+    def create_event
+           result = prompt.collect do
+            key(:name).ask('Event Name:')
+            key(:date).ask('Date:', value: "DD/MM/YYYY", convert: :datetime)
+            key(:time).ask('Time of Event:',value: "HH:MM", convert: :string)
+            # binding.pry
+            key(:price).ask('Price: $', convert: :int)
+        end
+        location_result = prompt.select("Please choose a location", Location.names)
+        location_instance = Location.find_by(name: location_result)
+        #### resul[:time] string to integer add to datetime 
+        hour = result[:time].split(":")[0].to_i
+        minute =result[:time].split(":")[1].to_i
+        event_datetime = result[:date].change(hour: hour, min: minute) #changes datetime's hour in create event
+        #  
+        e1 = Event.create(name: result[:name], date: event_datetime, price: result[:price],user_id: user.id, location_id: location_instance.id)
+        Participant.create(event_id: e1.id, user_id: user.id)
+        # Event.participatns should = 1
+        puts "NEW EVENT COMPLETED! Have fun at #{result[:name]} event!!!".colorize(:green)
+    end
+    
 end
